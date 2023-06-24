130e76171c9b72326e06db21756c94ec0a8011a9
brianjhudson <brianjasonhudson@gmail.com>
2023-06-24 10:11:31 -0500

    Add day one classwork and homework

---
+++ day_1/homework.mjs
@@ -0,0 +1,37 @@
+// 1. Declare variables for first, middle, and last names
+
+
+// 2. Concatenate the names and store them in a variable called fullName.
+
+
+// 3. Write a function that accepts first, middle, and last names and returns a full name.
+
+
+// 4. Create an object that represents a house for sale. Give it an address, a number of bedrooms, and a sale price.
+
+
+// 5. Console log just the price of the home.
+
+
+// 6. Add a boolean property to the house object to represent whether or not it is in a flood zone.
+
+
+// 7. Add $50k to the price.
+
+
+// 8. Loop through the properties of the house and log each one. 
+
+
+// 9. Create another house object with the same properties as the first.
+
+
+// 10. Create an array to store both houses.
+
+
+// 11. Loop through the array and log the price of each house.
+
+
+// 12. Loop through the array and log each property of each house. (That's two loops, one nested in the other.)
+
+
+// 13. Write a function that takes in a house object and a property name and returns the value for that property on the house object.
