d57778916c84044a1b24af70f21a2927d59f3113
Nicole Nguyen <121835728+nguyenlnicole@users.noreply.github.com>
2023-06-26 10:53:39 -0500

    Update homework.mjs

--- day_1/homework.mjs
+++ day_1/homework.mjs
@@ -1,20 +1,30 @@
 // 1. Declare variables for first, middle, and last names
+var firstNmae = 'Nicole'
+var middleName = 'Lan'
+var lastNAme = 'Nguyen'
 
 
 // 2. Concatenate the names and store them in a variable called fullName.
+var fullName = firstName + ' ' + middleName + ' ' + lastName;
+OR
+var fullName = `firstName: ${firstName}, middleName: ${middleName}, lastName: ${lastName}`;
 
 
 // 3. Write a function that accepts first, middle, and last names and returns a full name.
-
+console.log(FullName); //I do n0t undertsnad this one. Can we review this?
 
 // 4. Create an object that represents a house for sale. Give it an address, a number of bedrooms, and a sale price.
-
+var house = {
+  address:  'Indigo St',
+  bedRooms: 4,
+  salePrice: 450,000,
+}
 
 // 5. Console log just the price of the home.
-
+console.log(salePrice);
 
 // 6. Add a boolean property to the house object to represent whether or not it is in a flood zone.
-
+isInFloodZonn: false;
 
 // 7. Add $50k to the price.
 
@@ -23,10 +33,15 @@
 
 
 // 9. Create another house object with the same properties as the first.
-
+var house2 = {
+  address:  ' Fenchurch Dr',
+  bedRooms: 5,
+  salePrice: 430,000,
+}
 
 // 10. Create an array to store both houses.
-
+var houses = [house, house2]
+var users2 = [{}, {}]
 
 // 11. Loop through the array and log the price of each house.
 
